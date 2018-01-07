8db33b587981540b6412d070aad287bac18a05bd
demo <gitpratice@gmail.com>
2018-01-07 09:17:48 +0530

    firstfile

---
+++ sum.java
@@ -0,0 +1,15 @@
+[A[C[C[C[C[C[C[C[C[C[C[C[C["cat>sum.java"
+class sum
+{
+[A[A[B[Bpublic static void main (string[] args)
+{
+{
+int a,b,c
+a=60;
+b=90;
+c=180;
+a=b+c+a;
+}
+system.out.println ("the sum of three values +c");
+}
+}
